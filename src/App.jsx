48293ba537d28a7f36b8d3cb5a4a0fb07afb9c26
Chirsth Peralta <christhperalta@gmail.com>
2023-03-31 22:02:35 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//api
+//https://api.dictionaryapi.dev/api/v2/entries/en/Know
