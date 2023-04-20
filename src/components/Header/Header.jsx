2b47345b0c13e1c60ce72ff85aca7f0a1e7c9317
Chirsth Peralta <christhperalta@gmail.com>
2023-04-20 01:27:56 -0400

    Updating components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { Icon } from "../common/Icon";
 import "./Header.scss";
 
-export const Header = ({ handleToggle, themeActive }) => {
+export const Header = ({ handleToggle, darkTheme }) => {
   return (
     <header className="Header">
       <Logo />
-      <div className="Header-container">
-        <DropDown themeActive={themeActive} />
-        <div className="Header-vertical-line"></div>
-        <ToggleSwitch handleToggle={handleToggle} themeActive={themeActive} />
-      </div>
+      <Interactive handleToggle={handleToggle} darkTheme={darkTheme} />
     </header>
   );
 };
@@ -17,123 +14,74 @@ export const Header = ({ handleToggle, themeActive }) => {
 export const Logo = () => {
   return (
     <h1 className="Header-h1">
-      <a href="#" className="Header-a">
-        <img src="assets/images/logo.svg" alt="logo" />
+      <a className="Header-a" href="">
+        <img className="Header-img" src="assets/images/logo.svg" alt="Logo" />
       </a>
     </h1>
   );
 };
 
-export const DropDown = ({ themeActive }) => {
-  const [isActive, setIsActive] = useState(false);
-  const [font, setFont] = useState("serif");
-
-  const handleToggle = () => {
-    setIsActive(!isActive);
-  };
-
-  const handleFontsTypes = (type) => {
-    setFont(type);
-  };
-
-  useEffect(() => {
-    const body = document.querySelector("body");
-
-    if (font === "sansSerif") {
-      body.classList.remove("inconsolata") || body.classList.remove("serif");
-      body.classList.add("sansSerif");
-    }
-    if (font === "serif") {
-      body.classList.remove("inconsolata") ||
-        body.classList.remove("sansSerif");
-      body.classList.add("serif");
-    }
-
-    if (font === "inconsolata") {
-      body.classList.remove("sansSerif") || body.classList.remove("serif");
-      body.classList.add("inconsolata");
-    }
-  }, [font]);
-
+export const Interactive = ({ handleToggle, darkTheme }) => {
   return (
-    <div className="Header-dropdown">
-      <button className="Header-dropdown-button" onClick={handleToggle}>
-        {font === "sansSerif" && "Sant Serif"}
-        {font === "serif" && "Serif"}
-        {font === "inconsolata" && "Mono"}
-        {themeActive ? (
-          <img
-            className={`Header-arrow ${isActive ? "isActive" : ""}`}
-            src="assets/images/icon-arrow-down-m.svg"
-            alt="arrow-down"
-          />
-        ) : (
-          <img
-            className={`Header-arrow ${isActive ? "isActive" : ""}`}
-            src="assets/images/icon-arrow-down.svg"
-            alt="arrow-down"
-          />
-        )}
-      </button>
-
-      <div
-        className={`Header-dropdown-div ${
-          isActive ? "isActive" : "isInActive"
-        } ${themeActive ? "theme" : ""}`}
-      >
-        <ul>
-          <li className="Header-li">
-            <button
-              className="Header-dropdown-ul-button sansSerif"
-              onClick={() => handleFontsTypes("sansSerif")}
-            >
-              Sans Serif
-            </button>
-          </li>
-          <li className="Header-li">
-            <button
-              className="Header-dropdown-ul-button serif"
-              onClick={() => handleFontsTypes("serif")}
-            >
-              Serif
-            </button>
-          </li>
-          <li className="Header-li">
-            <button
-              className="Header-dropdown-ul-button incorsolata"
-              onClick={() => handleFontsTypes("inconsolata")}
-            >
-              Mono
-            </button>
-          </li>
-        </ul>
-      </div>
+    <div className="Header-interactive">
+      <DropDown darkTheme={darkTheme} />
+      <div className="Header-bar"></div>
+      <ToggleSwitch handleToggle={handleToggle} darkTheme={darkTheme} />
+      <Icon
+        name="moon"
+        clase="Header-moon"
+        color={darkTheme ? "#A445ED" : "#838383"}
+      />
     </div>
   );
 };
 
-export const ToggleSwitch = ({ handleToggle, themeActive }) => {
+export const ToggleSwitch = ({ handleToggle, darkTheme }) => {
   return (
-    <div className="Header-themes">
-      <label className="Header-switch">
-        <input className="Hader-input" type="radio" onClick={handleToggle} />
-        <span
-          className={`Header-slider ${themeActive ? "isActive" : ""}`}
-        ></span>
-      </label>
-      {themeActive ? (
-        <img
-          className="Header-moon"
-          src="assets/images/icon-moon-m.svg"
-          alt="icon-moon"
+    <label className="Header-switch">
+      <input className="Header-input" type="checkbox" onClick={handleToggle} />
+      <span className={`Header-slider ${darkTheme ? "isActive" : ""}`}></span>
+    </label>
+  );
+};
+
+export const DropDown = ({ darkTheme }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const onClickActive = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className="Header-dropDown">
+      <button className="Header-button" onClick={onClickActive}>
+        <span className="Header-span">Sans Serif</span>
+        <Icon
+          name="arrow"
+          clase="Header-arrow"
+          color={darkTheme ? "#A445ED" : "#838383"}
         />
-      ) : (
-        <img
-          className="Header-moon"
-          src="assets/images/icon-moon.svg"
-          alt="icon-moon"
-        />
-      )}
+      </button>
+
+      <div
+        className={`Header-div ${isActive ? "isActive" : ""}
+        ${darkTheme ? "darkTheme shadow " : ""}
+      `}
+      >
+        <ul className="Header-ul">
+          <li className="Header-li">
+            <button className="Header-li-button">Sans Serif</button>
+          </li>
+
+          <li className="Header-li">
+            <button className="Header-li-button">Serif</button>
+          </li>
+
+          <li className="Header-li">
+            <button className="Header-li-button">Mono</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
